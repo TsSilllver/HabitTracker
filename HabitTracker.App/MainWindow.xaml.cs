c11e22bd5c025ed51043c863ac705a1e20ed4720
konstantin podlasov <komih51-51@hotmail.com>
2026-03-06 23:50:40 +0700

    Добавил базовую модель привычек и протестировал пользовательский интерфейс.

--- HabitTracker.App/MainWindow.xaml.cs
+++ HabitTracker.App/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using HabitTracker.Core.Models;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HabitTracker.Core.Models;
+
 
 namespace HabitTracker.App
 {
@@ -19,6 +22,18 @@ namespace HabitTracker.App
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var habits = new List<Habit>
+            {
+            new Habit { Name = "Встать в 6 утра", TargetValue = 1, Unit = "раз" },
+            new Habit { Name = "Подтягивания", TargetValue = 30, Unit = "раз" },
+            new Habit { Name = "Вода", TargetValue = 4, Unit = "литра" }
+             };
+            HabitsListBox.ItemsSource = habits;
+            HabitsListBox.DisplayMemberPath = "Name"; 
         }
     }
 }
